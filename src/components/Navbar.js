6e8e91a71ed48ec8a5610f525290541d1ddc48d1
meet21 <54228011+meet21@users.noreply.github.com>
2021-04-02 14:15:30 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,36 @@ import styled from 'styled-components';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
-  const {isAuthenticated, loginWithRedirect, logout, user} = useAuth0();
+  const {
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+    user,
+    isLoading,
+  } = useAuth0();
   const isUser = isAuthenticated && user;
 
-
-  // console.log(isAuthenticated,isLoading,user);
-  return <Wrapper>
-    {isUser && user.picture && <img src={user.picture} alt={user.name} />}
-    {isUser && user.name && <h4>Welcome,<strong>{user.name.toUpperCase()}</strong></h4>}
-    {isUser ?<button className="" onClick={()=>{logout({returnTo:window.location.origin})}}>Logout</button>:<button onClick={loginWithRedirect}>Login</button>}
-    {/* <button onClick={()=>{logout({returnTo:window.location.origin})}}>Logout</button> */}
-  </Wrapper>;
+  return (
+    <Wrapper>
+      {isUser && user.picture && <img src={user.picture} alt={user.name} />}
+      {isUser && user.name && (
+        <h4>
+          Welcome, <strong>{user.name.toUpperCase()}</strong>
+        </h4>
+      )}
+      {isUser ? (
+        <button
+          onClick={() => {
+            logout({ returnTo: window.location.origin });
+          }}
+        >
+          logout
+        </button>
+      ) : (
+        <button onClick={loginWithRedirect}>login</button>
+      )}
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.nav`
