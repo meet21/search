c7dda48de10851dad49c5c4f952ea9fe149da273
Ecell-official <meet21bhalgamiya@gmail.com>
2021-04-02 13:46:53 +0530

    Updates

---
+++ precache-manifest.68d9ac29620930d2fffb5798af7be84d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "725678e3f5282ed893bf",
+    "url": "/search/static/css/main.089431c1.chunk.css"
+  },
+  {
+    "revision": "725678e3f5282ed893bf",
+    "url": "/search/static/js/main.0230ffc3.chunk.js"
+  },
+  {
+    "revision": "744fc3f6f63fe8fae126",
+    "url": "/search/static/js/runtime~main.9c2b4df0.js"
+  },
+  {
+    "revision": "5ea77324bd2aad7491e6",
+    "url": "/search/static/js/2.f2088683.chunk.js"
+  },
+  {
+    "revision": "c114fda3847c2b52236772df2f17cd01",
+    "url": "/search/static/media/preloader.c114fda3.gif"
+  },
+  {
+    "revision": "ed463e555920fbfd98f76663abb2653b",
+    "url": "/search/static/media/login.ed463e55.jpg"
+  },
+  {
+    "revision": "fcb0c7d06186197ddff73e78f7aa5776",
+    "url": "/search/index.html"
+  }
+];
