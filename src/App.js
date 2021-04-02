097fd288f70495f1007996972973f522cefd7116
Ecell-official <meet21bhalgamiya@gmail.com>
2021-04-02 13:34:15 +0530

    undo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages/Index';
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages/index';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
